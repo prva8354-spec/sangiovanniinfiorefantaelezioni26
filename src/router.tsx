5d9c01149781010fd57fa1c9f0fc470624ddfe99
prva8354-spec <prva8354@gmail.com>
2026-06-01 11:41:46 +0200

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,8 @@
 import { createRouter } from '@tanstack/react-router'
-
-// Import the generated route tree
 import { routeTree } from './routeTree'
 
-// Create a new router instance
-export const getRouter = () => {
-  const router = createRouter({
-    routeTree,
-    scrollRestoration: true,
-    defaultPreloadStaleTime: 0,
-  })
-
-  return router
-}
+export const router = createRouter({
+  routeTree,
+  scrollRestoration: true,
+  defaultPreloadStaleTime: 0,
+})
